7bcec1dc72b95010fa5e191667cbde87b9d754f1
marigameo <mariappangameo@gmail.com>
2019-03-20 00:44:56 +0530

    Updates

---
+++ precache-manifest.523951caebd6e85f4c83944995c0a432.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "024ede4e56a7ea6a4090",
+    "url": "/Tasker/static/js/runtime~main.5cd0db9d.js"
+  },
+  {
+    "revision": "411508d4c33b155ed013",
+    "url": "/Tasker/static/js/main.414fc7ed.chunk.js"
+  },
+  {
+    "revision": "578be1edce25b38dbf5a",
+    "url": "/Tasker/static/js/2.7850536c.chunk.js"
+  },
+  {
+    "revision": "411508d4c33b155ed013",
+    "url": "/Tasker/static/css/main.fe2cdd3f.chunk.css"
+  },
+  {
+    "revision": "75a12000e0f3544d2da1273c670bf32d",
+    "url": "/Tasker/index.html"
+  }
+];
