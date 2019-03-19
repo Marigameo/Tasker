0b7d65d8c36f3d05a13b695788366bce1dcf8454
marigameo <mariappangameo@gmail.com>
2019-03-19 23:43:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import "./App.css";
+import Header from "./components/layout/Header";
+import About from "./components/pages/about";
+import uuid from "uuid";
+import Axios from "axios";
 class App extends Component {
+  state = {
+    todos: []
+  };
+
+  componentDidMount() {
+    Axios.get("https://jsonplaceholder.typicode.com/todos?_limit=5").then(res =>
+      this.setState({ todos: res.data })
+    );
+  }
+
+  //Toggle completed
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  //delete todo
+  delTodo = id => {
+    Axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res =>
+      this.setState({
+        todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      })
+    );
+    // console.log(id);
+    //... spread operator
+  };
+
+  //add todo
+  addTodo = title => {
+    // const newTodo = {
+    //   id: uuid.v4(),
+    //   title: title,
+    //   //in ES6 if key and value are the same then we can simply put as title
+    //   completed: false
+    // };
+    Axios.post("https://jsonplaceholder.typicode.com/todos", {
+      title,
+      completed: false
+    }).then(res => this.setState({ todos: [...this.state.todos, res.data] }));
+  };
+
   render() {
+    //console.log(this.state.todos);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          {/* take the todos items from the state and pass them as props to the todos component */}
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodos={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/about.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function about() {
+  return (
+    <React.Fragment>
+      <br />
+      <p>This is just a simple todo app build with react.js.</p>
+      <br />
+      <p>
+        This could be a simple app but a good one to start with as I learned
+        about all the basics of react.) that's cool
+      </p>
+      <br />
+      <p>React components</p>
+      <p>Virtual DOM</p>
+      <p>State and props</p>
+      <p>HTTP requests</p>
+      <p>ES6 syntaxes</p>
+      <p>React Router</p>
+      <br />
+      <p>
+        Note: It doesn't have a backend though it communicates with an API
+        backend that serves some random todos
+      </p>
+      <p>It makes http requests to the server</p>
+    </React.Fragment>
+  );
+}
